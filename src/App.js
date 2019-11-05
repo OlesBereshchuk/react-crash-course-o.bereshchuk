71635a8e1303449b67d35577dc2ef4adbf15348d
Oles Bereshchuk <o.bereshchuk@astoundcommerce.com>
2019-11-05 13:44:36 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from './components/Button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div>
+            <Button/>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
