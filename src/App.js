1099ccb6b170a53657bd28fd27fe912229855dad
Oles Bereshchuk <o.bereshchuk@astoundcommerce.com>
2019-11-12 10:25:55 +0200

    rewrite component with hooks

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import Button from './components/Button';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <div>
-            <Button/> 
-          </div>
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div className="App">
+        <div>
+          <Button/> 
+        </div>
+    </div>
+  );
 }
 
 export default App;
